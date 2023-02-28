d1739a577e0e396a4051f08d867a1639313b4722
thinkySide <113565086+thinkySide@users.noreply.github.com>
2023-02-28 17:34:28 +0900

    [Update] CoreBluetooth 기본 셋팅
    - info.plist 권한 설정
    - CBCentralManger 객체 생성 및 케이스 나열

--- CoreBluetooth-Lab/ViewController.swift
+++ CoreBluetooth-Lab/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
+    
+    var centralManager: CBCentralManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // CBCentralManager 초기화
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
 
 
+}
+
+extension ViewController: CBCentralManagerDelegate {
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        
+        // Central(발신자) 상태에 따른 케이스
+        switch central.state {
+            
+        case .unknown:
+            print("central.state: .unknonw")
+        case .resetting:
+            print("central.state: .resetting")
+        case .unsupported:
+            print("central.state: .unsupported")
+        case .unauthorized:
+            print("central.state: .unauthorized")
+        case .poweredOff:
+            print("central.state: .poweredOff")
+        case .poweredOn:
+            print("central.state: .poweredOn")
+        @unknown default:
+            print("central.state: default case")
+        }
+        
+    }
+    
 }
 
